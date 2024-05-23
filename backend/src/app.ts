ea78779e3b35b7e31df66cd9b6b75f922a8bc1bd
boraneak <boraneak123@gmail.com>
2024-05-23 14:04:13 +0700

    init backend

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
