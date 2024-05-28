503b8ee4c8cc45aaa0be23ebb22b5e24d3bd9125
boraneak <boraneak123@gmail.com>
2024-05-29 04:04:12 +0700

    add util

---
+++ backend/utils/isValidObjectId.ts
@@ -0,0 +1,14 @@
+import { Response } from "express";
+import mongoose from "mongoose";
+
+export const isValidObjectId = (
+  id: string,
+  res: Response,
+  modelName: string
+): boolean => {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ error: `Invalid ${modelName} id format` });
+    return false;
+  }
+  return true;
+};
