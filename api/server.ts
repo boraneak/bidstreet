0ce2687a1a21da9775834f5dd08682b3a803415d
boraneak <boraneak123@gmail.com>
2024-09-22 17:57:20 +0700

    just a tiny update

--- api/server.ts
+++ api/server.ts
@@ -1,14 +1,13 @@
 import { connectToDatabase } from './database';
 import app from './src/app';
 import { config } from 'config/config';
-const PORT = config.port;
 
-app.listen(PORT, async () => {
+app.listen(config.port, async () => {
   const currentDate = new Date().toLocaleString();
   console.log('Server Info:');
   console.log(`Environment: ${config.nodeEnv}`);
   await connectToDatabase();
   console.log(
-    `Server is listening at http://localhost:${PORT} on ${currentDate}`,
+    `Server is listening at http://localhost:${config.port} on ${currentDate}`,
   );
 });
