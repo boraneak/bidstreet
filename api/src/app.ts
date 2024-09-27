d2679f0e2b86f7355d6fc8a3e091a3b0cd4db81b
boraneak <boraneak123@gmail.com>
2024-09-27 22:37:24 +0700

    generate csrf token

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
@@ -8,6 +8,7 @@ import { config } from 'config/config';
 import { specs } from '../openApi';
 import { rateLimiter, expressSession } from '../middlewares';
 import lusca from 'lusca';
+import { ICSRFTokenRequest } from 'interfaces/requests/CSRFTokenRequest';
 
 const app = express();
 
@@ -27,6 +28,11 @@ app.use(lusca.csrf());
 
 app.use('/api/v1', router);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+app.get('/csrf-token', (req: Request, res: Response) => {
+  const csrfToken = (req as ICSRFTokenRequest).csrfToken();
+  res.json({ csrfToken });
+});
 
 app.get('/', (_req, res) => {
   res.send('Welcome to the Express server!');
