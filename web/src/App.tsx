51d83ca6eece301a62fab285b76f03a40424f050
boraneak <boraneak123@gmail.com>
2024-10-03 22:03:20 +0700

    add vercel analytics

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { Router } from './AppRouter';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Router />
       </BrowserRouter>
+      <Analytics />
     </div>
   );
 }
